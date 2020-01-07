a52ca957fbf214fbcfb5fba42ffa00e2ddad941a
Schrödinger's Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2020-01-07 20:07:03 +0100

    Bitwig: add package scripts

---
+++ Bitwig/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'Bitwig'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64  = 'https://downloads.bitwig.com/stable/3.1.1/Bitwig%20Studio%203.1.1.msi'
+$checksum64 = '9425620f13bad2d542c67a0d0327564448d75a1ee8c6610082efee72e6abf67a'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url64bit      = $url64
+  softwareName  = 'Bitwig Studio' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ Bitwig/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+function global:au_GetLatest {
+    $releases = "https://www.bitwig.com/en/previous_releases.html"
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+    $regex   = '.msi$'
+    $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+    $url     = "https:" + $url
+    $url     = $url.Replace(" ", "%20")
+    $version = $url.Split('%20') | Select-Object -Last 1
+    $version = $version.Replace(".msi", "")
+    # Workaround because AU changes package ID in the nuspec to the folder name without asking
+    $global:workaroundPackageName = (Split-Path -Leaf $PSScriptRoot).ToLower()
+    return @{ Version = $version; URL64 = $url; PackageName = $workaroundPackageName }
+}
+
+function global:au_BeforeUpdate() {
+   $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update -ChecksumFor none
