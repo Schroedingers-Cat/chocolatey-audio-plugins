ad5b864ae6cf19a8d6e7dd5bb963ce6fc6b4ff78
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2022-09-24 23:42:08 +0200

    Add Voxengo TEQ-421

---
+++ Voxengo/TEQ-421/tools/chocolateyvariables.ps1
@@ -0,0 +1,53 @@
+﻿$packageName    = 'TEQ-421'
+$company        = 'Voxengo'
+$softwareName   = "$company $packageName"
+$url32          = 'https://www.voxengo.com/files/VoxengoTEQ421_10_Win32_64_VST_VST3_AAX_setup.exe'
+$releases       = 'https://www.voxengo.com/product/teq421/'
+$checksum32     = 'fcbc20e7ea8a2edcd4fe447487bad183c63a8995095731c7b82effe0d0334167'
+$global:companyPath    = "${env:PROGRAMFILES}\$company"
+$global:vst2Path       = "${env:PROGRAMFILES}\Steinberg\VSTPlugins\$company"
+$global:vst2x86_64Path = "${env:ProgramFiles(x86)}\Steinberg\VSTPlugins\$company"
+
+function CreateRegistryObjects () {
+  # The installer does not have an option for custom paths so we need to create the registry entry before
+  $global:regKeys =
+  @{'path'="HKLM:\Software\Voxengo\AudioPluginsInstall"; 'key'="DirVST2_64";  'value'="$vst2Path";        'bit'=64;    'validpp'="NoVst2x64"},
+  @{'path'="HKLM:\Software\Voxengo\AudioPluginsInstall"; 'key'="DirVST2_32";  'value'="$vst2x86BitAware"; 'bit'=64,32; 'validpp'="NoVst2x86"}
+}
+function CreateRegistryFileObjects () { $global:regKeyFileObjects }
+function CreateShortcutObjects () { $global:shortcuts }
+function CreateSymlinkObjects () {
+  if($pp["CompanyPath"] -ne $false) {
+    $global:linkUserDataSupport = @{'linkPath'="$standardCompanyPath"; 'linkName'=""; 'destPath'=$companyPath; 'bit'=64,32; 'validpp'="NoVst2x64", "NoVst2x86", "NoVst3x64", "NoVst3x86", "NoAaxx86", "NoAaxx64"}
+    $global:symlinks = $linkUserDataSupport
+  } else {
+    $global:symlinks
+  }
+}
+function CreateInstallerObjects () {
+  $global:installerComponentsList =
+  @{'value'="shortcuts"; 'bit'=64,32; 'validpp'="NoShortcuts"},
+  @{'value'="guides";    'bit'=64,32; 'validpp'="NoVst2x64", "NoVst2x86", "NoVst3x64", "NoVst3x86", "NoAaxx86", "NoAaxx64"},
+  @{'value'="vst2_32";   'bit'=64,32; 'validpp'="NoVst2x86"},
+  @{'value'="vst2_64";   'bit'=64;    'validpp'="NoVst2x64"},
+  @{'value'="vst3_32";   'bit'=64,32; 'validpp'="NoVst3x86"},
+  @{'value'="vst3_64";   'bit'=64;    'validpp'="NoVst3x64"},
+  @{'value'="aax_32";    'bit'=64,32; 'validpp'="NoAaxx86"},
+  @{'value'="aax_64";    'bit'=64;    'validpp'="NoAaxx64"}
+}
+function CreatePackageRessourcePathObjects () { $global:PackageRessourcePathList }
+function CreateTxtFileObjects () { $global:PackageNewFiles }
+function CreatePackageParametersObjects () {
+  $global:packageArgs = @{
+    packageName   = $packageName
+    unzipLocation = $unzPath
+    fileType      = 'exe'
+    url           = $url32
+    url64bit      = $url32
+    softwareName  = "$company $packageName*" #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+    checksum      = $checksum32
+    checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+    silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  }
+  $global:packageParametersObjectsList = $packageArgs
+}
