70014e996da443e79c2219d30a7f9b1ab7d377ae
Schroedingers-Cat <Schroedingers-Cat@users.noreply.github.com>
2024-09-29 05:53:49 +0000

    Apply Choco package update - MadronaLabs\Sumu

--- MadronaLabs/Sumu/tools/chocolateyinstall.ps1
+++ MadronaLabs/Sumu/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://madronalabs-earlyaccess.s3.amazonaws.com/SumuInstaller1.0.0b15.exe'
+$url64      = 'https://madronalabs-earlyaccess.s3.amazonaws.com/SumuInstaller1.0.0b20.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
   softwareName   = 'Sumu version*'
 
   checksumType64 = 'sha256'
-  checksum64     = 'a4cf0388d76ebf671afed2414c3b3913c3809408eb523641b2adf40564d733d1'
+  checksum64     = 'a4cf0388d76ebf671afed2414c3b3913c3809408eb523641b2adf40564d733d4'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   validExitCodes = @(0)
 }
