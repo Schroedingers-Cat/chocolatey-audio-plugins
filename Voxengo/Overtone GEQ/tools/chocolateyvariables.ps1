67bef2439c2fb1b84ba568a412d960cbbb636542
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2024-10-29 14:37:52 +0100

    Revert "Apply Choco package update - Voxengo\Overtone GEQ"
    This reverts commit 562ba7094aa6a1a446430add9bec2dae27584d4a.

--- Voxengo/Overtone GEQ/tools/chocolateyvariables.ps1
+++ Voxengo/Overtone GEQ/tools/chocolateyvariables.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName    = 'Overtone GEQ'
 $company        = 'Voxengo'
 $softwareName   = "$company $packageName"
-$url32          = 'https://www.voxengo.com/files/VoxengoOvertoneGEQ_118_Win32_64_VST_VST3_AAX_setup.exe'
+$url32          = 'https://www.voxengo.com/files/VoxengoOvertoneGEQ_117_Win32_64_VST_VST3_AAX_setup.exe'
 $releases       = 'https://www.voxengo.com/product/overtonegeq/'
-$checksum32     = 'e6181c8e0abda07eae3e4357c21a1692ddb7f7cc2191282261fb91fdb5d75412'
+$checksum32     = '2df43ed20f0f2fd5bc8177be51f7c50e5d8c5b5543f7ad8ee4ba58e3de1b8739'
 
 function CreateRegistryObjects () {
   # The installer does not have an option for custom paths so we need to create the registry entry before
