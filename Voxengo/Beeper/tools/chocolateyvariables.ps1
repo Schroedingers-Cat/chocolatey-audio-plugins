71c21545b6d4b8cf91dc91ac3b22576f68b6018f
Schroedingers-Cat <Schroedingers-Cat@users.noreply.github.com>
2024-10-31 22:37:17 +0000

    Apply Choco package update - Voxengo\Beeper

--- Voxengo/Beeper/tools/chocolateyvariables.ps1
+++ Voxengo/Beeper/tools/chocolateyvariables.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = 'Beeper'
 $company        = 'Voxengo'
 $softwareName   = "$company $packageName"
-$checksum       = '2f255b2092a791e1b0daef0f64c25e95ecd8b4446a92bf2c30973a76b174c033'
-$url            = 'https://www.voxengo.com/files/VoxengoBeeper_212_Win32_64_VST_VST3_AAX_setup.exe'
+$checksum       = 'd8d1bc9cbdaeffa0e41df038e39bd371a7dbe3c0dcc6d7e7722629f87c817879'
+$url            = 'https://www.voxengo.com/files/VoxengoBeeper_213_Win32_64_VST_VST3_AAX_setup.exe'
 
 function CreateRegistryObjects () {
   # The installer does not have an option for custom paths so we need to create the registry entry before
