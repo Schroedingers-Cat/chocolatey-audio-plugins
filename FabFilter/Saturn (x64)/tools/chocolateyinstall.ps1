5b215ffea9ac51b5eb436651dabf5808a0531e53
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2025-02-27 16:40:42 +0000

    Apply Choco package update - FabFilter\Saturn (x64)

--- FabFilter/Saturn (x64)/tools/chocolateyinstall.ps1
+++ FabFilter/Saturn (x64)/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
     fileType       = 'exe'
-    url            = 'https://cdn-b.fabfilter.com/downloads/ffsaturn100x64.exe'
+    url            = 'https://www.fabfilter.com/downloads/ffsaturn129x64.exe'
     softwareName   = 'FabFilter Saturn (x64)*'
     checksumType   = 'sha256'
-    checksum       = '24228C5BF884C53C702AFBF593634CE28A4AE3E4D542138269ECCE03B48A8FCD'
+    checksum       = 'aa555f75f79c835a71902b9a1342f4d11b6c318e03a67cef6676df613174c33a'
     silentArgs     = '/Unattended' # FabFilter Installer
     validExitCodes = @(0)
 }
