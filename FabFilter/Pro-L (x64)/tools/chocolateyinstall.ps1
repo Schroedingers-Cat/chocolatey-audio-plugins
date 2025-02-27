5572a80e79b61b4591e28ee4e946355959d572d5
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2025-02-27 16:29:35 +0000

    Apply Choco package update - FabFilter\Pro-L (x64)

--- FabFilter/Pro-L (x64)/tools/chocolateyinstall.ps1
+++ FabFilter/Pro-L (x64)/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
     fileType       = 'exe'
-    url            = 'https://cdn-b.fabfilter.com/downloads/ffprol100x64.exe'
+    url            = 'https://www.fabfilter.com/downloads/ffprol135x64.exe'
     softwareName   = 'FabFilter Pro-L (x64)*'
     checksumType   = 'sha256'
-    checksum       = '24228C5BF884C53C702AFBF593634CE28A4AE3E4D542138269ECCE03B48A8FCD'
+    checksum       = '77e6d642b55e416d95014f4682b392c47ad601e97191473cc9291ddae21fc687'
     silentArgs     = '/Unattended' # FabFilter Installer
     validExitCodes = @(0)
 }
