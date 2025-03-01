f0444d4dab17dd2cbab6ce586ba5fc23fe6ed883
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2025-03-02 00:56:42 +0100

    Melda: Add MPluginManager

---
+++ MeldaProduction/MPluginManager/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://www.meldaproduction.com/downloads/down?name=MPluginManager_01_19_setup.exe&platform=win&version=01.19&mirror=bunnycdn&url=https%3A%2F%2Fmeldaproduction.b-cdn.net%2Fdownload%2FMPluginManager_01_19_setup.exe&checksum=cdd04f9fd53cbc40c739752c4eff465c60e8ebdf'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'MPluginManager*'
+  checksum64    = '3a66a8b700e8e5c9b75d2ac08bd3fc1a8157ffbb40ef58a90c3493dc481d9eff'
+  checksumType64= 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+# Package Parameters
+$pp = Get-PackageParameters
+
+if ($pp['InstallationPath']) { 
+  $installPath = $pp['InstallationPath']
+  $packageArgs.silentArgs += " /Dir=`"${installPath}`"" 
+}
+
+if ($pp['NoDesktopIcon']) { 
+  $packageArgs.silentArgs += " /TASKS=" 
+} else {
+  $packageArgs.silentArgs += " /TASKS=desktopicon"
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ MeldaProduction/MPluginManager/update.ps1
@@ -0,0 +1,61 @@
+import-module Chocolatey-AU
+
+$releases = 'https://www.meldaproduction.com/downloads'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*\`$url64\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+        ".\mpluginmanager.nuspec" = @{
+            "(?i)(^\s*<version>).*(<\/version>)" = "`${1}$($Latest.Version)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regex = "MPluginManager_\d\d_\d\d_setup\.exe"
+    $url64 = ($download_page.links | Where-Object href -match $regex | Select-Object -First 1 -ExpandProperty href).Trim()
+
+    if (-not $url64) {
+        throw "Installer URL not found on the releases page."
+    }
+
+    # Ensure the URL is absolute
+    if ($url64 -notmatch '^https?://') {
+        $baseUri = [System.Uri]$releases
+        $url64 = [System.Uri]::new($baseUri, $url64).AbsoluteUri
+    }
+
+    # Decode URL so &amp; becomes &
+    $url64 = [System.Net.WebUtility]::HtmlDecode($url64)
+
+    $versionRegex = "MPluginManager_(\d\d)_(\d\d)_setup\.exe"
+    if ($url64 -match $versionRegex) {
+        $versionMajor = $matches[1]
+        $versionMajor = $versionMajor -replace '^0+', ''
+        $versionMinor = $matches[2]
+        $versionMinor = $versionMinor -replace '^0+', ''
+        $version = "${versionMajor}.${versionMinor}"
+    }
+
+    Write-Host "Version is " $version
+    Write-Host "URL is $url64"
+
+    return @{
+        URL64          = $url64
+        Version        = $version
+        ChecksumType64 = 'sha256'
+        PackageName    = "meldaproduction-mpluginmanager"
+        SoftwareName   = "MeldaProduction MPluginManager"
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
+}
+
+update -ChecksumFor none
