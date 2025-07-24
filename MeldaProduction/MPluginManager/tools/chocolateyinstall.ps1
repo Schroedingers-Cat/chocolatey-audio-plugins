ed4b257ed1b1ffbe5e18873e462054fb3320a1aa
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2025-07-24 01:09:44 +0000

    Consolidated commit from package updates

--- MeldaProduction/MPluginManager/tools/chocolateyinstall.ps1
+++ MeldaProduction/MPluginManager/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://www.meldaproduction.com/downloads/down?name=MPluginManager_02_10_setup.exe&platform=win&version=02.10&mirror=bunnycdn&url=https%3A%2F%2Fmeldaproduction.b-cdn.net%2Fdownload%2Fmpluginmanager%2FMPluginManager_02_10_setup.exe&checksum=2bd73b8c99d34a4a30ead4d6677973b727aadb33'
+$url64      = 'https://www.meldaproduction.com/downloads/down?name=MPluginManager_02_11_setup.exe&platform=win&version=02.11&mirror=bunnycdn&url=https%3A%2F%2Fmeldaproduction.b-cdn.net%2Fdownload%2Fmpluginmanager%2FMPluginManager_02_11_setup.exe&checksum=494f8567f6bf293171bc9c59fc88fa03c5bd2ed7'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'MPluginManager*'
-  checksum64    = 'c61d03e73fae2ebdbbd178f07e9854590cedc66b8397eaef834ba1d3fc2085bb'
+  checksum64    = '2fb4082f40021a60ac55c83ab5544f336131b6fe3dc7935b6403e794cfd3567e'
   checksumType64= 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
