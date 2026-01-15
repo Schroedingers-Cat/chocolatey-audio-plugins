0a414d11c9cdf5758dc91ffcd70200781be4816a
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2026-01-15 23:43:44 +0100

    Add FabFilter Pro-C 3

---
+++ FabFilter/Pro-C 3 (x64)/tools/chocolateyinstall.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = 'https://www.fabfilter.com/downloads/ffproc300x64.exe'
+    softwareName   = 'FabFilter Pro-C 3 (x64)*'
+    checksumType   = 'sha256'
+    checksum       = '48b3dbdeb5045c14782fee17b0c8ee1a038057381b040570031cf67b3a8c9620'
+    silentArgs     = '/Unattended' # FabFilter Installer
+    validExitCodes = @(0)
+}
+
+# FabFilter installers support customizing VST2 path via registry key
+$registryPath  = 'HKCU:\Software\FabFilter'
+$paths = @{
+    "x64" = "VstPluginsPath64"
+    "x86" = "VstPluginsPath32"
+}
+$registryValue = $paths["x64"]
+
+$pp = Get-PackageParameters
+$vst2Path = $pp['Vst2Path']
+
+if (($vst2Path) -And (Test-Path -Path $pp['Vst2Path'] -IsValid)) {
+    if (!(Test-Path $registryPath)) {
+        New-Item -Path $registryPath | Out-Null
+    }
+    Set-ItemProperty -Path $registryPath -Name $registryValue -Value $pp['Vst2Path'] -Type String -Force
+}
+elseif ((Test-Path $registryPath) -And ((Get-ItemProperty -Path $registryPath).PSObject.Properties.Name -contains $registryValue)) {
+    Remove-ItemProperty -Path $registryPath -Name $registryValue -Force
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ FabFilter/Pro-C 3 (x86)/tools/chocolateyinstall.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = 'https://www.fabfilter.com/downloads/ffproc300.exe'
+    softwareName   = 'FabFilter Pro-C 3 (x86)*'
+    checksumType   = 'sha256'
+    checksum       = 'dfd76c52437a9be496c3fea4918ee760980b3f8ea081281b1dc70655e4ccc87f'
+    silentArgs     = '/Unattended' # FabFilter Installer
+    validExitCodes = @(0)
+}
+
+# FabFilter installers support customizing VST2 path via registry key
+$registryPath  = 'HKCU:\Software\FabFilter'
+$paths = @{
+    "x64" = "VstPluginsPath64"
+    "x86" = "VstPluginsPath32"
+}
+$registryValue = $paths["x86"]
+
+$pp = Get-PackageParameters
+$vst2Path = $pp['Vst2Path']
+
+if (($vst2Path) -And (Test-Path -Path $pp['Vst2Path'] -IsValid)) {
+    if (!(Test-Path $registryPath)) {
+        New-Item -Path $registryPath | Out-Null
+    }
+    Set-ItemProperty -Path $registryPath -Name $registryValue -Value $pp['Vst2Path'] -Type String -Force
+}
+elseif ((Test-Path $registryPath) -And ((Get-ItemProperty -Path $registryPath).PSObject.Properties.Name -contains $registryValue)) {
+    Remove-ItemProperty -Path $registryPath -Name $registryValue -Force
+}
+
+Install-ChocolateyPackage @packageArgs
