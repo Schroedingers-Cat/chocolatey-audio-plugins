44ad897794bdee87da4b524e8672325ac42fc7b9
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2026-05-11 23:12:33 +0200

    Reason: Add Reason v14 package

---
+++ ReasonStudios/Reason 14/tools/chocolateyinstall.ps1
@@ -0,0 +1,47 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$unzipPath  = Join-Path -Path $toolsDir -ChildPath "ReasonInstaller"
+New-Item -Path $unzipPath -ItemType Directory -Force
+
+$url64 = 'https://cdn.reasonstudios.com/update/Stable/Reason_1400_d381-Stable-825-Win.zip'
+$url64NoSB = 'https://cdn.reasonstudios.com/update/Stable/Reason_1400_d381-Stable-825-without_soundbanks-Win.zip'
+$checksum64 = '13582d25e12c654c48cdcc59bcd44fed12c983b01b9941178c979cadd7aef99a'
+$checksum64NoSB = '7e598e6c2126e982ad537bca2223315b232a3c78443470c601cde6511bc2617a'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $unzipPath
+  fileType       = 'exe'
+  url64bit       = $url64
+  file64         = "${unzipPath}\Install Reason 14.exe"
+  softwareName   = 'Reason 14 14.*'
+  checksumType64 = 'sha256'
+  checksum64     = $checksum64
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  validExitCodes = @(0)
+}
+
+# Package Parameters
+$pp = Get-PackageParameters
+
+if ($pp['InstallationPath']) { 
+  $installPath = $pp['InstallationPath']
+  $packageArgs.silentArgs += " /Dir=`"${installPath}`"" 
+}
+if ($pp['NoDesktopIcon']) { $packageArgs.silentArgs += " /TASKS=" }
+if ($pp['WithoutSoundbanks']) { 
+  $packageArgs.url64bit =  $url64NoSB
+  $packageArgs.checksum64 =  $checksum64NoSB
+}
+
+$componentsList = "standalone,vst,aax"
+if ($pp['NoVst']) { $componentsList = $componentsList -replace ",vst","" }
+if ($pp['NoAax']) { $componentsList = $componentsList -replace ",aax","" }
+$packageArgs.silentArgs += " /Components=${componentsList}"
+
+# Run Install
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+
+# Cleanup
+Remove-Item $unzipPath -Recurse -Force -ErrorAction SilentlyContinue
