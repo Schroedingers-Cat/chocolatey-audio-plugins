df7f55b83642f2babf282743f4e519a10179019d
Schroedingers-Cat <6382400+Schroedingers-Cat@users.noreply.github.com>
2026-05-11 23:38:16 +0200

    u-he: Add Zebra3

---
+++ u-he/Zebra3/tools/chocolateyuninstall.ps1
@@ -0,0 +1,41 @@
+﻿$ErrorActionPreference = 'Stop';
+$installerType = 'EXE'
+
+$silentArgs = '/qn /norestart'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+if ($installerType -ne 'MSI') {
+  $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  $validExitCodes = @(0)
+}
+
+$packageNameFull = 'Zebra3'
+$version = '3.0.0'
+$packageName = $packageNameFull.Replace(" ", "")
+$softwareName = "$packageName ${version}"
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $file = "$($_.UninstallString)"
+
+    if ($installerType -eq 'MSI') {
+      $silentArgs = "$($_.PSChildName) $silentArgs"
+      $file = ''
+    }
+
+    Uninstall-ChocolateyPackage -PackageName $softwareName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$softwareName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
